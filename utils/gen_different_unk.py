540b2b248721dc46c83b4fa6b2d58d0ba88cbf19
xin zhao <focusxin@outlook.com>
2022-08-13 21:57:14 +0800

    update

--- utils/gen_different_unk.py
+++ utils/gen_different_unk.py
@@ -3,8 +3,14 @@ import os
 
 if __name__ == '__main__':
     domains = ["Art", "Clipart", "Product", "Real"]
+    domain_name = {
+        "Art": "art",
+        "Clipart": "clipart",
+        "Product": "product",
+        "Real": "real_world"
+    }
     # mode: univ or obda
-    mode = 'univ'
+    mode = 'obda'
 
     path = osp.join('data', domains[0])
     dir_list = os.listdir(path)
@@ -14,16 +20,16 @@ if __name__ == '__main__':
     if mode == 'univ':
         n_share = 10
         n_source_private = 5
+        unknown_nums = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
     elif mode == 'obda':
-        n_share = 20
+        n_share = 25
         n_source_private = 0
+        unknown_nums = [5, 10, 15, 20, 25, 30, 35, 40]
     else:
         raise NotImplemented
 
     n_source = n_share + n_source_private
     source_list = dir_list[:n_source]
-
-    unknown_nums = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 
     for target_domain in domains:
         p_path = os.path.join('data', target_domain)
@@ -31,12 +37,11 @@ if __name__ == '__main__':
             target_list = dir_list[:n_share] + dir_list[n_source:n_source +
                                                         unknown_num]
             path_target = "txt_unknown/target_{}_{}_{}.txt".format(
-                target_domain.lower(), mode, unknown_num)
+                domain_name[target_domain], mode, unknown_num)
             with open(path_target, "w") as f:
                 for k, direc in enumerate(target_list):
                     if not '.txt' in direc:
                         files = os.listdir(os.path.join(p_path, direc))
-                        files.sort()
                         for i, file in enumerate(files):
                             file_name = os.path.join('data', target_domain,
                                                      direc, file)
